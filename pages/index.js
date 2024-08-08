4d251b203d67baea4d4082a01d9544f735ef59d9
tejth <ptejendra91@gmail.com>
2024-08-08 16:24:23 +0530

    add link component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,18 @@ export default function Home() {
       </Head>
       <nav className={styles.mainnav}>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Blog</li>
-          <li>Contact</li>
+          <Link href={"/"}>
+            <li>Home</li>
+          </Link>
+          <Link href={"/about"}>
+            <li>About</li>
+          </Link>
+          <Link href={"/blog"}>
+            <li>Blog</li>
+          </Link>
+          <Link href={"/contact"}>
+            <li>Contact</li>
+          </Link>
         </ul>
       </nav>
       <main className={`${styles.main} ${inter.className}`}>
