c5e1a3f6f6dec7e254875e46a5f33a386bc27e8f
tejth <ptejendra91@gmail.com>
2024-08-10 08:04:15 +0530

    fix styling

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -14,10 +14,13 @@ const Slug = (props) => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1>{blog && blog.title}</h1>
-        <hr />
+        <h1 className={styles.title}>{blog && blog.title}</h1>
+        <hr className={styles.separator} />
         {blog && (
-          <div dangerouslySetInnerHTML={createMarkup(blog.content)}></div>
+          <div
+            className={styles.content}
+            dangerouslySetInnerHTML={createMarkup(blog.content)}
+          ></div>
         )}
       </main>
     </div>
@@ -44,4 +47,5 @@ export async function getStaticProps(context) {
     props: { myBlog: JSON.parse(myBlog) }, // will be passed to the page component as props
   };
 }
+
 export default Slug;
