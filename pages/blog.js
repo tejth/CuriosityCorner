1ec3cbeca2f3b03743a3e4cc6df43cdf495a3f03
tejth <ptejendra91@gmail.com>
2024-08-10 05:35:34 +0530

    using dangerously set inner html

--- pages/blog.js
+++ pages/blog.js
@@ -1,23 +1,17 @@
 import React, { useEffect, useState } from "react";
 import styles from "../styles/Blog.module.css";
 import Link from "next/link";
+import * as fs from "fs";
 
 // Step 1: Collect all the files from blogdata directory
 // Step 2: Iterate through the and Display them
 
-const Blog = () => {
-  const [blogs, setBlogs] = useState([]);
-  useEffect(() => {
-    console.log("useeffect is running");
-    fetch("http://localhost:3000/api/blogs")
-      .then((a) => {
-        return a.json();
-      })
-      .then((parsed) => {
-        setBlogs(parsed);
-      });
-  }, []);
+const Blog = (props) => {
+  console.log(props);
+  const [blogs, setBlogs] = useState(props.allBlogs);
+  // useEffect(() => {
 
+  // }, [])
   return (
     <div className={styles.container}>
       <main className={styles.main}>
@@ -28,7 +22,7 @@ const Blog = () => {
                 <h3 className={styles.blogItemh3}>{blogitem.title}</h3>
               </Link>
               <p className={styles.blogItemp}>
-                {blogitem.content.substr(0, 140)}...
+                {blogitem.metadesc.substr(0, 140)}...
               </p>
             </div>
           );
@@ -38,9 +32,16 @@ const Blog = () => {
   );
 };
 
-export async function getServerSideProps(context) {
-  let data = await fetch("http://localhost:3000/api/blogs");
-  let allBlogs = await data.json();
+export async function getStaticProps(context) {
+  let data = await fs.promises.readdir("blogdata");
+  let myfile;
+  let allBlogs = [];
+  for (let index = 0; index < data.length; index++) {
+    const item = data[index];
+    console.log(item);
+    myfile = await fs.promises.readFile("blogdata/" + item, "utf-8");
+    allBlogs.push(JSON.parse(myfile));
+  }
 
   return {
     props: { allBlogs }, // will be passed to the page component as props
